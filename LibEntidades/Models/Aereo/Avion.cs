88dead12e4d2db14cf48fda08fecd082071391e6
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-15 00:24:55 -0300

    tp grupal terminado

---
+++ LibEntidades/Models/Aereo/Avion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibEntidades.Models.BASE;
+
+namespace LibEntidades.Models.BASE
+{
+    public class Avion : Aereo 
+    {
+        public Avion() { }
+
+        public Avion(string nombre, string modelo, int anio, int capPasajeros, string modoVuelo, string funcionalidad, string propietario, string aerolinea):base(nombre, modelo, anio, capPasajeros, modoVuelo, funcionalidad)
+        {
+
+            Propietario = propietario;
+
+            Aerolinea = aerolinea;
+
+        }
+
+        public string Propietario { get; set; }
+
+        public string Aerolinea { get; set; }
+    }
+}
